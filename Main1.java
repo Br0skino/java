bf28cb0e97747eef3c8e0a33857f7099171ac0f7
BroskinoLamer <62884274+BroskinoLamer@users.noreply.github.com>
2021-10-05 14:10:29 +0200

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Inserisci un numero:");
+		int a=0;
+		Scanner sc=new Scanner(System.in);
+		int numero= sc.nextInt();
+		while(a!=numero){
+			System.out.print("#");
+			a++;
+			
+		}	
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
